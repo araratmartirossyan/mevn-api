1cf62501af6bfa3b4c5860e62569302260747922
Ararat <ararat.martirossyan@jumbo.com>
2020-09-02 12:28:35 +0200

    Created order

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,18 +1,35 @@
 const dollarsToCents = require('dollars-to-cents');
-
+const { Order } = require('../model')
+const { sum } = require('ramda')
 const stripe = require('stripe')(
   'sk_test_51H0jsgJjMphy3gtYka8ebhPtKrWueI9YK3doRTKSShU2ACKGJsCbDZVcvnZrSHaxunwfUo2WnPsNBV0O0hCmldL100xbJGcaY0',
 );
 
-const createPaymentIntent = async ({ body: { amount } }, res) => {
+const createPaymentIntent = async ({ body: { fullname, address, phone, email, products } }, res) => {
   try {
+
+    if (!address) {
+      throw new Error('Адрес обязателен')
+    }
+
+    const amount = sum(products.map(i => Number(i.amount)))
+    const prepareOrder = {
+      fullname, address, phone, email, products, amount
+    }
+
+    const newOrder = await new Order(prepareOrder)
+    const saveOrder = newOrder.save()
+
     const paymentIntent = await stripe.paymentIntents.create({
       amount: dollarsToCents(amount),
       currency: 'eur',
       payment_method_types: ['card'],
     });
 
-    return res.status(200).send(paymentIntent);
+    return res.status(200).send({
+      paymentIntent,
+      saveOrder
+    });
   } catch (err) {
     res.status(500).send(err);
   }
@@ -20,7 +37,10 @@ const createPaymentIntent = async ({ body: { amount } }, res) => {
 
 const stripeWebHook = async ({ body }, res) => {
   try {
-  } catch (err) {}
+    console.log(body)
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
 module.exports = {
