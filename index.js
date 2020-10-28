487362b60674c74a10c7d1fe4184c104ad8301a4
Icwt Admin <ararat@incodewetrust.ru>
2020-10-28 09:33:21 +0000

    Added JWT and Logout

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const http = require("http");
 const cors = require("cors");
 const { routes } = require("./src/routes");
+const checkJWTSign = require('./src/middlewares/jwtCheck.middleware')
 // настроим подключение к бд
-mongoose.connect("mongodb+srv://ararat:maxt9leo123@cluster0.skcm1.gcp.mongodb.net/test?retryWrites=true&w=majority", {
+mongoose.connect(process.env.MONGO_URI, {
   useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -14,7 +16,9 @@ mongoose.connect("mongodb+srv://ararat:maxt9leo123@cluster0.skcm1.gcp.mongodb.ne
 // инициализируем приложение
 const app = express();
 app.use(cors());
+
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 routes.forEach((item) => {
--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,10 +1,11 @@
 const router = require("express-promise-router")();
 
+const {checkJWTSign} = require('../middlewares/jwtCheck.middleware')
 const { category } = require("../controllers");
 
 router.route("/:id").get(category.get);
 router.route("/").post(category.create);
-router.route("/").get(category.getAll);
+router.route("/").get(checkJWTSign, category.getAll);
 router.route("/:id").put(category.update);
 router.route("/:id").delete(category.delete);
 
